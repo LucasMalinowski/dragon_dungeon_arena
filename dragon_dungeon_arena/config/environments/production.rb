aaaf151b994c243fd551d1acc8b14fdd389638b9
lucasbrazau <lucasbrazau@fetch.ly>
2024-04-10 21:17:40 -0300

    Polish Lobby page

--- dragon_dungeon_arena/config/environments/production.rb
+++ dragon_dungeon_arena/config/environments/production.rb
@@ -21,13 +21,13 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  # config.public_file_server.enabled = false
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
